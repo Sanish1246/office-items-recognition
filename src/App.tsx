c51a6c07ad5caf99b96846162f2cc6b7e9dcde81
Sanish1246 <Sanishferrari@gmail.com>
2025-10-09 18:56:55 +0400

    Making top section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
-import "./App.css";
+import { Button } from "@/components/ui/button";
+import { Upload, Camera } from "lucide-react";
 
 function App() {
+  const [choice, setChoice] = useState("Upload");
   return (
     <>
-      <h1>Test</h1>
+      <div className="w-screen text-center border-1 mx-auto">
+        <h1 className="mt-5 text-2xl">Office Items Recognition</h1>
+        <p className="mt-10">Choose an option</p>
+        <div className="flex flex-row justify-center mt-5">
+          <Button variant="outline" onClick={() => setChoice("Upload")}>
+            <Upload />
+            Upload
+          </Button>
+          <Button variant="outline" onClick={() => setChoice("Webcam")}>
+            <Camera />
+            Webcam
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
